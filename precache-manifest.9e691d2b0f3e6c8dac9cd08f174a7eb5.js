a72943c3f7cd4b1307e9b877dd783640b3eb3d8d
Vladimir Ovechkin <ovchingus@gmail.com>
2019-10-26 18:14:32 -0700

    Initial commit

---
+++ precache-manifest.9e691d2b0f3e6c8dac9cd08f174a7eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0783ee5ad62b80f7675a58c843929871",
+    "url": "/interactive-cv/index.html"
+  },
+  {
+    "revision": "ac2b15d7911513e68e5f",
+    "url": "/interactive-cv/static/css/main.50997d52.chunk.css"
+  },
+  {
+    "revision": "3b982bb9939ee8154cd8",
+    "url": "/interactive-cv/static/js/2.bd8317f1.chunk.js"
+  },
+  {
+    "revision": "ac2b15d7911513e68e5f",
+    "url": "/interactive-cv/static/js/main.eabc1e5e.chunk.js"
+  },
+  {
+    "revision": "10d10629eb19fe6616f3",
+    "url": "/interactive-cv/static/js/runtime-main.5b0b2c54.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/interactive-cv/static/media/logo.25bf045c.svg"
+  }
+]);
